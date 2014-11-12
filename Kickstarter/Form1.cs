a32f34a507f3129f1a803cb1a70daa6577e8071f
mgosselinUTC <mgosselin@utc4me.org>
2014-11-12 13:30:05 -0500

    better states, data to csv

--- Kickstarter/Form1.cs
+++ Kickstarter/Form1.cs
@@ -10,43 +10,82 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Kickstarter.Api.Model;
 
 namespace Kickstarter
 {
     public partial class Form1 : Form
     {
+        private IKickstarterSession session;
+        private string[] states;
+
+        private async void login()
+        {
+            var client = new KickstarterClient();
+            session = await client.StartSession("kickstarter@utc4me.org", "utc4medotorg");
+            Console.WriteLine("logged in!");
+        }
+
+        private async void buildStates()
+        {
+            StreamReader reader = new StreamReader("states.txt");
+            string line = "";
+            states = new string[50];
+            int counter = 0;
+            while ((line = reader.ReadLine()) != null)
+            {
+                states[counter] = line;
+                counter++;
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            login();
+            buildStates();
+        }
+
+        private int getIDforState(string state)
+        {
+            return Array.IndexOf(states, state) + /*alabama... */ 2347560;
+        }
+
+        private async Task<IEnumerable<Project>> getProjectsFrom(string state, int take)
+        {
+            return await session.Query(new DiscoverProjects().Woe("" + getIDforState(state)).SortedBy("newest").Take(take));
         }
 
         private async void button1_Click(object sender, EventArgs e)
         {
-            string state = textBox1.Text;
+            //int stateID = getIDforState(textBox1.Text);
 
-            var client = new KickstarterClient();
+            IEnumerable<Project> projects = await getProjectsFrom(textBox1.Text, 500);
 
-            var session = await client.StartSession("kickstarter@utc4me.org", "utc4medotorg");
+            StreamWriter writer = new StreamWriter("C:\\Users\\mgosselin\\Desktop\\stuff.csv");
+            writer.WriteLine("Project Name, Location,, Status, Pledged, Goal, Backers, Launch Date");
 
-            StreamReader reader = new StreamReader("states.txt");
-            string line = "";
-            string[] states = new string[50];
-            int counter = 0;
-            while((line = reader.ReadLine()) != null) {
-                states[counter] = line;
-                counter ++;
+            foreach (Project project in projects)
+            {
+             
+                string location = project.Location.DisplayableName.Replace(",", "");
+                string place = location.Substring(0, location.Length - 3);
+                string stateAbbr = location.Substring(location.Length - 2);
+                location = place + "," + stateAbbr;
+
+                writer.WriteLine(String.Join(", ", new string[] {
+                
+                    project.Name.Replace(",", ""),
+                    location,
+                    project.State.Replace(",", "").Replace("\"", "\\\""),
+                    "" + project.Pledged,
+                    "" + project.Goal,
+                    "" + project.BackersCount,
+                    project.LaunchedAt.Date.ToString().Replace(",", " "),
+
+                }));
             }
-
-            int stateID = Array.IndexOf(states, state) + /*alabama... */ 2347559;
-
-            var query = new DiscoverProjects()
-                          .InStatus("live")
-                          .SortedBy("launch_date")
-                          .Take(200)
-                          .Woe("" + stateID);
-
-            Console.WriteLine(from p in await session.Query(query) orderby p.LaunchedAt select p);
-
+            Close();
         }
     }
 }
