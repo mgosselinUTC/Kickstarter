cc7f48d7f02c8d3feb84003ce35d463fd29fb8e7
mgosselinUTC <mgosselin@utc4me.org>
2014-11-14 12:00:36 -0500

    no idea, icon trouble

--- Kickstarter/Form1.Designer.cs
+++ Kickstarter/Form1.Designer.cs
@@ -141,7 +141,6 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::Kickstarter.Properties.Resources.kickstarter_logo_k_color;
             this.pictureBox1.InitialImage = null;
             this.pictureBox1.Location = new System.Drawing.Point(174, 12);
             this.pictureBox1.Name = "pictureBox1";
